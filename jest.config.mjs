4323949d08b1696a28296d9b5f7d2c5296533678
Christopher Rodrigues <rethissunstar@gmail.com>
2025-03-11 11:28:14 -0400

    added configurations for tailwind and structure to follow DDD

---
+++ jest.config.mjs
@@ -0,0 +1,17 @@
+import nextJest from 'next/jest';
+
+const createJestConfig = nextJest({ dir: './' });
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/domain/(.*)$': '<rootDir>/src/domain/$1',
+    '^@/application/(.*)$': '<rootDir>/src/application/$1',
+    '^@/infrastructure/(.*)$': '<rootDir>/src/infrastructure/$1',
+    '^@/presentation/(.*)$': '<rootDir>/src/presentation/$1',
+  },
+};
+
+export default createJestConfig(customJestConfig);
+
